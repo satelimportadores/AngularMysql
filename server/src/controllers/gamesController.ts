7d8fbb080215a5874a9822d2706a151782d373a5
Juan <satelimportadores@gmail.com>
2020-08-14 10:49:32 -0500

    primera consulta

--- server/src/controllers/gamesController.ts
+++ server/src/controllers/gamesController.ts
@@ -1,8 +1,10 @@
 import {Request, Response} from 'express';
+import pool from '../database';
 
 class GamesController{
  public index  (req : Request,res: Response){
-    res.send('Games')
+    pool.query('DESCRIBE games');
+    res.json('games');
   } 
 }
 
