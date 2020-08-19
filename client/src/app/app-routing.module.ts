ca2ecc866b123834d1f295d2eae0263a3d7e06bd
Juan <satelimportadores@gmail.com>
2020-08-19 11:15:22 -0500

    Rutas de navegacion

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GamaListComponent } from './components/gama-list/gama-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',redirectTo:'/games', pathMatch: 'full'
+  },
+  {
+    path: 'games', component: GamaListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
